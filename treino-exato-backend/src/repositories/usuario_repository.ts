1842f0988b0f1dbb3a13aedce9b0c7d3475a0f6b
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2026-06-23 15:47:55 -0300

    refactor(backend): comentado os códigos para melhor entendimento e aprendizado

--- treino-exato-backend/src/repositories/usuario_repository.ts
+++ treino-exato-backend/src/repositories/usuario_repository.ts
@@ -1,13 +1,37 @@
+// Importa a instância do Supabase configurada para interagir com o banco de dados
 import supabase from '../config/supabase.js';
 
-export async function buscarUsuarioPorEmail(
-    email: string
-) {
+/** Função responsável por buscar um usuário no banco de dados pelo email fornecido.
+ * 
+ * Recebe um email como parâmetro, realiza uma consulta no banco de dados usando o Supabase
+ * para encontrar um usuário com o email correspondente e retorna os dados do usuário se encontrado.
+ * 
+ * @param email Email do usuário a ser buscado no banco de dados
+ * @returns     Dados do usuário encontrado se houver correspondência. Lança um erro caso contrário
+ */
+export async function buscarUsuarioPorEmail(email: string){
+    /** 1: Realiza uma consulta no banco de dados usando o Supabase para buscar um usuário com o email fornecido.
+     * 
+     * A consulta seleciona todos os campos da tabela 'usuario' 
+     * e filtra os resultados para encontrar um registro com o email correspondente. 
+     * 
+     * A função retorna os dados do usuário encontrado ou lança um erro caso não haja correspondência.
+     * 
+     * 'from':   Especifica a tabela do banco de dados a ser consultada, neste caso, a tabela 'usuario';
+     * 'select': Define os campos a serem retornados na consulta, neste caso, todos os campos ('*');
+     * 'eq':     Filtra os resultados para encontrar um registro com o email correspondente.
+     * 'single': Indica que a consulta deve retornar apenas um registro, caso haja correspondência.
+     * 
+     * @param data  Dados do usuário encontrado no banco de dados, caso haja correspondência
+     * @param error Objeto de erro retornado pelo Supabase caso ocorra algum problema na consulta
+     * @param email Email do usuário a ser buscado no banco de dados, fornecido como parâmetro da função
+     * @returns     Dados do usuário encontrado se houver correspondência. Lança um erro caso contrário
+     */
     const { data, error } = await supabase.from('usuario').select('*').eq('email', email).single();
 
-    if(error){
-        throw error;
-    }
+    // 2: Verifica se ocorreu algum erro durante a consulta ao banco de dados.
+    if(error) {throw error;}
 
+    // 3: Retorna os dados do usuário encontrado no banco de dados, caso haja correspondência.
     return data;
 }
--- treino-exato-backend/src/routes/login_routes.ts
+++ treino-exato-backend/src/routes/login_routes.ts
@@ -1,8 +1,11 @@
-import { Router } from 'express';
-import { login } from '../controllers/login_controller.js';
+import { Router } from 'express';                               // Importa o módulo Router do Express para criar rotas
+import { login } from '../controllers/login_controller.js';     // Importa a função de login do controlador de login
 
+// 1: Cria uma instância do Router para definir as rotas relacionadas ao login
 const router = Router();
 
+// 2: Define a rota POST para o endpoint '/login' e associa a função de login a essa rota
 router.post('/login', login);
 
+// 3: Exporta o router para ser usado em outros arquivos do projeto
 export default router;
